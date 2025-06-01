bdb68ba0f671b7e9c388b340578ed99976011317
Suvham Paul <suvham_paul@epam.com>
2025-06-01 16:51:23 +0530

    k8s folder

---
+++ task8b/modules/k8s/variablest.tf
@@ -0,0 +1,19 @@
+variable "acr_login_server" {
+  type = string
+}
+
+variable "docker_image_name" {
+  type = string
+}
+
+variable "kv_name" {
+  type = string
+}
+
+variable "redis_hostname_secret_name" {
+  type = string
+}
+
+variable "redis_password_secret_name" {
+  type = string
+}
---
+++ task8b/modules/k8s/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "~> 2.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
