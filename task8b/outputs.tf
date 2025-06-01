3b585cce852dc06552b522e2e1bd5a568c5db6e2
Suvham Paul <suvham_paul@epam.com>
2025-06-01 16:43:19 +0530

    aci ipaddr

--- task8b/outputs.tf
+++ task8b/outputs.tf
@@ -5,3 +5,7 @@ output "aks_lb_ip" {
   value       = data.kubernetes_service.k8_service.status[0].load_balancer[0].ingress[0].ip
   description = "value"
 }
+
+output "aci_ip_address" {
+  value = module.aci-redis.aci_ip_address 
+}
